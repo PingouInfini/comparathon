dee9c776bfd2b32d190703f86d665b154e45d7ba
pingouInfini <pingouInfini@osef.fr>
2019-04-08 00:30:45 +0200

    Fix bash script

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 rm -rf ./docker/context/dockerdist/*.py
-cp -Rf requirements.txt ./docker/context/
-cp -Rf *.py ./docker/context/dockerdist
-cp -Rf google/*.* ./docker/context/dockerdist/google/
-cp -Rf face_reco/*.* ./docker/context/dockerdist/face_reco/
-cp -Rf extractor ./docker/context/dockerdist/extractor/
+mkdir -p ./docker/context/ && cp -Rf requirements.txt ./docker/context/
+mkdir -p ./docker/context/dockerdist && cp -Rf *.py ./docker/context/dockerdist
+mkdir -p ./docker/context/dockerdist/google/ && cp -Rf google/*.* ./docker/context/dockerdist/google/
+mkdir -p ./docker/context/dockerdist/face_reco/ && cp -Rf face_reco/*.* ./docker/context/dockerdist/face_reco/
+mkdir -p ./docker/context/dockerdist/extractor/ && cp -Rf extractor ./docker/context/dockerdist/extractor/
 cd docker
-docker-compose.exe -f ./comparathon.yml up -d --build
+docker-compose -f ./comparathon.yml up -d --build
