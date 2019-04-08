6eceeaaac792e3e431441ee1c3a065446012e459
pingouInfini <pingouInfini@osef.fr>
2019-04-08 10:53:43 +0200

    Add samples directory

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ xcopy "requirements.txt" ".\docker\context\" /Y /F
 xcopy "*.py" ".\docker\context\dockerdist" /Y /F
 xcopy "google\*.*" ".\docker\context\dockerdist\google\" /Y /F
 xcopy "face_reco\*.*" ".\docker\context\dockerdist\face_reco\" /Y /F
-xcopy "extractor" ".\docker\context\dockerdist\extractor\" /Y /E
+xcopy "extractor\*.*" ".\docker\context\dockerdist\extractor\" /Y /E
+xcopy "samples\*.*" ".\docker\context\dockerdist\samples\" /Y /E
 cd docker
 docker-compose.exe -f .\comparathon.yml up -d --build
--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ mkdir -p ./docker/context/ && cp -Rf requirements.txt ./docker/context/
 mkdir -p ./docker/context/dockerdist && cp -Rf *.py ./docker/context/dockerdist
 mkdir -p ./docker/context/dockerdist/google/ && cp -Rf google/*.* ./docker/context/dockerdist/google/
 mkdir -p ./docker/context/dockerdist/face_reco/ && cp -Rf face_reco/*.* ./docker/context/dockerdist/face_reco/
-mkdir -p ./docker/context/dockerdist/extractor/ && cp -Rf extractor ./docker/context/dockerdist/extractor/
+mkdir -p ./docker/context/dockerdist/extractor/ && cp -Rf extractor/* ./docker/context/dockerdist/extractor/
+mkdir -p ./docker/context/dockerdist/samples/ && cp -Rf samples/* ./docker/context/dockerdist/samples/
 cd docker
 docker-compose -f ./comparathon.yml up -d --build
