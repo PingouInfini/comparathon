8b427d794c7b33197e3bfc66fcfbf6ae7fd35b5d
PingouInfini <pingouInfini@gmail.com>
2019-04-17 23:12:04 +0200

    reorganization

--- comparathon.py
+++ comparathon.py
@@ -7,6 +7,25 @@ def usage():
     print("Usage:")
     print("python {} <username> <picture's filename>".format(sys.argv[0]))
 
+def get_relative_images_and_url(user, ref_image_path, limit=25):
+    #1 Get images from Google with username
+    my_args = {"keywords": user, "limit": limit,
+               "extract_metadata": True,
+               "output_directory": "mesimages",
+               "metadata_directory": "mesjson",
+               "type": "photo"
+               }
+
+    googleimages.get_images_from_arguments(my_args)
+
+
+    #2 Filter the gotten pictures with Face Recognition
+    source_dir = "mesimages/{}".format(user)
+    face_reco.pictures_filtering(ref_image_path, source_dir, user)
+
+    #3 Extract the filtered pictures associated urls
+    json_path="mesjson/{}.json".format(user)
+    extractor.json_parsing(json_path, user)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
@@ -14,23 +33,6 @@ if __name__ == '__main__':
         sys.exit(1)
 
     user = sys.argv[1]
-
-    #1 Get images from Google with username
-    my_args = {"keywords": user, "limit": 25,
-                "extract_metadata": True,
-                "output_directory": "mesimages",
-                "metadata_directory": "mesjson",
-                "type": "photo"
-                }
-
-    googleimages.get_images_from_arguments(my_args)
-
-
-    #2 Filter the gotten pictures with Face Recognition
     ref_image_path = sys.argv[2]
-    source_dir = "mesimages/{}".format(user)
-    face_reco.pictures_filtering(ref_image_path, source_dir, user)
 
-    #3 Extract the filtered pictures associated urls
-    json_path="mesjson/{}.json".format(user)
-    extractor.json_parsing(json_path, user)
+    get_relative_images_and_url(user,ref_image_path)
